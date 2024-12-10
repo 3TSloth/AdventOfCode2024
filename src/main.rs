345bfcb46001ee788fb67413b36cd4324142986a
Paulo Moreira <pdevsloth3@gmail.com>
2024-12-10 13:02:19 -0500

    Added solution for question 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -6,7 +7,7 @@ use std::io::BufReader;
 fn main() {
     println!("Starting program.");
     // let mut reader_handle = take_stdin();
-    question1();
+    question2();
 
     println!("Finished program.");
 }
@@ -62,7 +63,7 @@ fn question1() -> io::Result<()> {
 
     let total_distance: i32 = distances.iter().sum();
 
-    println!("Answer for quesiton 1 is: {}", total_distance);
+    println!("Answer for question 1 is: {}", total_distance);
 
     Ok(())
 }
@@ -70,15 +71,99 @@ fn question1() -> io::Result<()> {
 fn question2() {
     let reader = take_stdin();
 
-    let levels: Vec<Vec<i32>> = Vec::new();
+    let mut levels: Vec<Vec<i32>> = Vec::new();
 
     for line in reader.lines() {
         //Convert to array of numbers
         let line_unwrapped = line.unwrap();
-        let line_split: Vec<&str> = line_unwrapped
+        let mut line_split: Vec<i32> = line_unwrapped
             .split(' ')
-            .map(|num| num.parse::<i32>().unwrap());
+            .map(|num| num.parse::<i32>().unwrap())
+            .collect();
 
-        println("Line split is {?:}", line_split);
+        //Add an int at the end to act as a flag whether it is safe or unsafe
+        line_split.push(-1);
+
+        levels.push(line_split);
+
+        println!("Line split is {:?}", levels.get(0));
     }
+
+    //Now iterate over each one and check if it's always increasing
+
+    for level in levels.as_mut_slice() {
+        let mut index = 0;
+        let mut count = 0;
+
+        while index + 1 < level.len() - 1 {
+            let previous_num = level.get(index).unwrap();
+            let next_num = level.get(index + 1).unwrap();
+
+            println!("Gets in while loop");
+
+            let diff = (previous_num - next_num).abs();
+            let is_within_limits = diff >= 1 && diff <= 3;
+            let is_increasing = previous_num < next_num;
+            if is_within_limits && is_increasing {
+                count += 1;
+            }
+            index += 1;
+        }
+
+        if count == (level.len() - 2) {
+            *level.last_mut().unwrap() = 1;
+        } else {
+            *level.last_mut().unwrap() = -1;
+        }
+
+        println!("Count is: {}", count);
+        println!("Level is: {:?}", level);
+    }
+
+    //Now check if it's always decreasing
+
+    for level in levels.as_mut_slice() {
+        let mut index = 0;
+        let mut count = 0;
+
+        //Skip if we already know it's a valid increasing one from the previous scan
+        if *level.last().unwrap() == 1 {
+            continue;
+        }
+
+        while index + 1 < level.len() - 1 {
+            let previous_num = level.get(index).unwrap();
+            let next_num = level.get(index + 1).unwrap();
+
+            println!("Gets in while loop");
+
+            let diff = (previous_num - next_num).abs();
+            let is_within_limits = diff >= 1 && diff <= 3;
+            let is_decreasing = previous_num > next_num;
+            if is_within_limits && is_decreasing {
+                count += 1;
+            }
+            index += 1;
+        }
+
+        if count == (level.len() - 2) {
+            *level.last_mut().unwrap() = 1;
+        } else {
+            *level.last_mut().unwrap() = -1;
+        }
+
+        println!("Count is: {}", count);
+        println!("Level is: {:?}", level);
+    }
+
+    let mut num_safe_reports = 0;
+
+    for level in levels {
+        let is_safe_flag = *level.last().unwrap();
+        if is_safe_flag == 1 {
+            num_safe_reports += 1;
+        }
+    }
+    println!("Number of safe reports is: {}", num_safe_reports);
+    println!("Finished question 2");
 }
